9c71613007939e23fbc29549500467da188ac6d2
Daniel B <dantheman41@live.com.au>
2022-06-25 10:51:12 +1000

    Formatting, make camera point at gltf center

--- src/ModelViewer/viewer.js
+++ src/ModelViewer/viewer.js
@@ -3,45 +3,43 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x111111);
-    
-    const ambientLight = new THREE.AmbientLight(0xffffff, .7)
-    scene.add(ambientLight)
-    
-    const dirLight = new THREE.DirectionalLight(0xffffff, .8)
-    dirLight.position.set(3, 3, -3)
-    dirLight.castShadow = true
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x111111);
 
-    dirLight.shadow.camera.top = 2;
-    dirLight.shadow.camera.bottom = -2;
-    dirLight.shadow.camera.left = -2;
-    dirLight.shadow.camera.right = 2;
-    dirLight.shadow.camera.near = .02;
-    dirLight.shadow.camera.far = 10;
-    dirLight.shadow.bias = -0.003;
-    dirLight.shadow.mapSize.set(1024, 1024)
-    
-    scene.add(dirLight)
-    
-    
-    var floorgeometry = new THREE.CircleGeometry(2, 80);
-    var floormaterial = new THREE.MeshPhongMaterial({
-        color: 0xdddddd,
-        shininess: 20
-    });
+const ambientLight = new THREE.AmbientLight(0xffffff, .7)
+scene.add(ambientLight)
 
-    const floor = new THREE.Mesh(floorgeometry, floormaterial);
-    floor.rotation.x = -Math.PI * 0.5;
-    floor.position.z = 0;
-    floor.position.x = 0;
-    floor.position.y = 0;
-    floor.receiveShadow = true;
-    scene.add(floor);
+const dirLight = new THREE.DirectionalLight(0xffffff, .8)
+dirLight.position.set(3, 3, -3)
+dirLight.castShadow = true
+dirLight.shadow.radius = 3
 
-    let model = null
+dirLight.shadow.camera.top = 2;
+dirLight.shadow.camera.bottom = -2;
+dirLight.shadow.camera.left = -2;
+dirLight.shadow.camera.right = 2;
+dirLight.shadow.camera.near = .02;
+dirLight.shadow.camera.far = 10;
+dirLight.shadow.bias = -0.003;
+dirLight.shadow.mapSize.set(1024, 1024)
 
+scene.add(dirLight)
 
+const floorgeometry = new THREE.CircleGeometry(2, 80);
+const floormaterial = new THREE.MeshPhongMaterial({
+    color: 0xdddddd,
+    shininess: 20
+});
+
+const floor = new THREE.Mesh(floorgeometry, floormaterial);
+floor.rotation.x = -Math.PI * 0.5;
+floor.position.z = 0;
+floor.position.x = 0;
+floor.position.y = 0;
+floor.receiveShadow = true;
+scene.add(floor);
+
+var model = null
 
 export default function viewer(canvasRef) {
     const renderer = new THREE.WebGLRenderer({
@@ -57,24 +55,18 @@ export default function viewer(canvasRef) {
     camera.position.set(-1, 1, -2)
     
     const controls = new OrbitControls(camera, canvasRef)
+    // controls.autoRotate = true
+    // controls.autoRotateSpeed = 3
+    controls.enableDamping = true
+    controls.dampingFactor = .3
     controls.enablePan = false;
     controls.minDistance = 1;
-    controls.maxDistance = 10; 
-  
+    controls.maxDistance = 10;
+    
     var mixer = undefined
 
     const clock = new THREE.Clock()
     clock.start();
-
-    function update() {
-        const delta = clock.getDelta();
-        renderer.render(scene, camera)
-    
-        if (mixer)
-            mixer.update(delta)
-        
-        requestAnimationFrame(update);
-    }
     
     const gltfLoader = new GLTFLoader()
     
@@ -99,13 +91,14 @@ export default function viewer(canvasRef) {
                     node.castShadow = true;
                 }
             });
+
+            controls.target = new THREE.Box3().setFromObject(gltf.scene).getCenter(new THREE.Vector3())
             
             mixer = new THREE.AnimationMixer(gltf.scene)
             mixer.clipAction(gltf.animations[0]).play()
             scene.add(gltf.scene)
         })
         
-    update();
 
     const setModel = (file) => {
         const url = URL.createObjectURL(file);
@@ -124,9 +117,10 @@ export default function viewer(canvasRef) {
                         node.castShadow = true;
                     }
                 } );
+
+                controls.target = new THREE.Box3().setFromObject(gltf.scene).getCenter(new THREE.Vector3())
                 
                 mixer = new THREE.AnimationMixer(gltf.scene)
-                
                 mixer.clipAction(gltf.animations[0]).play()
                 
                 scene.add(gltf.scene)
@@ -146,12 +140,22 @@ export default function viewer(canvasRef) {
             if (node.isMesh) {
                 node.geometry.dispose()
                 node.material.dispose()
-                // node.dispose()
             }
         })
-        console.log(renderer.info)
-
     }
+
+    function update() {
+        const delta = clock.getDelta();
+        renderer.render(scene, camera)
+        
+        controls.update(delta)
+    
+        if (mixer)
+            mixer.update(delta)
+        
+        requestAnimationFrame(update);
+    }
+    update();
 
     return {setModel, dispose}
 }
