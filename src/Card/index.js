310c77ed66950846d0b99c9db4a07306e50fd2d1
Daniel B <dantheman41@live.com.au>
2022-08-03 13:28:40 +1000

    Show images in cards

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 function Card(props) {
+    const [markerURL, setMarkerURL] = useState(`/api/marker/${props.character.id}`);
+
     return (
         <div className={`cursor-pointer w-64 h-96 p-8 m-8 bg-neutral-200 dark:bg-neutral-800 rounded-lg drop-shadow ring-8 ring-sky-500 ${props.selected ? 'ring-2' : 'ring-0'}`} onClick={props.onClick}>
             <h2 className='text-neutral-900 dark:text-white font-bold text-2xl break-words mb-4 min-h-[2rem]'>{props.character.name}</h2>
@@ -16,6 +18,7 @@ function Card(props) {
                     </li>)
                 }) : null}
             </ul>
+            <img src={markerURL} alt="" width="200px" className="rounded"></img>
         </div>
     );
 }
