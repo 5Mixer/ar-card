13b39aa1bd861e2c6aa3a0643f3c2ac3f4e07500
Daniel B <dantheman41@live.com.au>
2022-09-20 23:27:41 +1000

    Home redesign

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home(props) {
   return (
-    <div className="dark:bg-neutral-900 bg-neutral-50 subpixel-antialiased">
-        <div className="flex">
-            <section className="shrink w-full p-8 rounded flex full-row flex-wrap h-screen overflow-auto">
-                <h1 className="text-xl text-white">Home</h1>
-            </section>
-        </div>
+    <div className="dark:bg-neutral-900 bg-neutral-50 subpixel-antialiased container">
+        <section className="shrink w-full p-8 rounded h-screen overflow-auto">
+            <h1 className="text-5xl text-gray-900 dark:text-white block mb-10">Augmented Reality Portal</h1>
+            
+            <p>
+                <Link to="listing" className="text-gray-900 font-bold dark:hover:text-amber-400 hover:text-amber-900">Open</Link>
+            </p>
+        </section>
     </div>
   );
 };
